9b7d742937e833b78cad64663aef4be78afd182b
phduo <phduo@DUONGPHAM>
2024-07-01 08:56:45 +0700

    xoa libs khong dung den

--- src/components/MenuVerticalComponent/VerticalMenu.jsx
+++ src/components/MenuVerticalComponent/VerticalMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './VerticalMenu.css';
 
 const VerticalMenu = () => {
--- src/pages/DanhSachSanPham/ProductList.jsx
+++ src/pages/DanhSachSanPham/ProductList.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import products from "../SanPham/Product"; 
 import VerticalMenu from "../../components/MenuVerticalComponent/VerticalMenu"; 
