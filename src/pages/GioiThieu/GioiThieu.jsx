2329615d71b77b61dc98b23739f97b6aa5432c6c
duong pham <phduongc1tantrao2018@gmail.com>
2024-07-04 19:09:56 +0700

    xoa bot thu vien khong dung

--- src/pages/GioiThieu/GioiThieu.jsx
+++ src/pages/GioiThieu/GioiThieu.jsx
@@ -1,40 +1,8 @@
 import Banner from "../../components/BannerComponent/Banner";
-import { useLocation } from "react-router-dom";
 import "./GioiThieu.css";
 import ProductList from "../DanhSachSanPham/ProductList";
-import { useEffect } from "react";
 
 const GioiThieu = () => {
-  // const location = useLocation();
-
-  // useEffect(() => {
-  //   const param = new URLSearchParams(location.search);
-  //   const verifyCode = param.get("verifyCode");
-  //   console.log(verifyCode);
-  //   const verifyAccount = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://localhost:7039/api/GuestManager/verify-cus?verifyCode=${verifyCode}`,
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //         }
-  //       );
-  //       if (response.ok) {
-  //         console.log("ok");
-  //       } else {
-  //         console.log("co loi");
-  //       }
-  //     } catch (error) {
-  //       console.error("Co loi xay ra: " + error);
-  //     }
-  //   };
-
-  //   verifyAccount();
-  // }, [location]);
-
   const isBlockTrue = true;
   return (
     <div className="containerGt">
