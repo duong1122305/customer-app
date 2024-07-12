6a45d4dadeba8be05daa196bfbd6bc6dbeb6d9cb
phduo <phduo@DUONGPHAM>
2024-07-12 17:03:53 +0700

    them anh luc bi loi xac minh

--- src/pages/Register/Duplicate.jsx
+++ src/pages/Register/Duplicate.jsx
@@ -1,6 +1,10 @@
 const Duplicate = () => {
   return (
     <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKg3bfRHzonWw2fDidvndhJSW5pUPHt6sYxw&s"
+        alt="Error"
+      />
       <h1>Tài khoản của bạn đã có hoặc mã xác minh không chính xác</h1>
       <p>Vui lòng liên hệ tới hotline của MEWSHOP để được hỗ trợ!!!!</p>
     </div>
--- src/pages/Register/Expired.jsx
+++ src/pages/Register/Expired.jsx
@@ -1,6 +1,10 @@
 const Expired = () => {
   return (
     <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKg3bfRHzonWw2fDidvndhJSW5pUPHt6sYxw&s"
+        alt="Error"
+      />
       <h1>
         Đã quá hạn xác minh !!! Mời bạn đăng ký lại để nhận mã xác minh mới.
       </h1>
