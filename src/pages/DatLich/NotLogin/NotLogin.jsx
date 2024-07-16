25ecc08a2dfd2b02e179adbf4f3054f271edcc17
duong pham <phduongc1tantrao2018@gmail.com>
2024-07-16 22:13:14 +0700

    login notlogin booking

--- src/pages/DatLich/NotLogin/NotLogin.jsx
+++ src/pages/DatLich/NotLogin/NotLogin.jsx
@@ -1,10 +1,56 @@
+import { Button, ButtonGroup, FloatingLabel, Form } from "react-bootstrap";
+import TableServices from "../TableServices/TableServices";
+import { useState } from "react";
+import "./NotLogin.css";
 
 const NotLogin = () => {
-    return (
-        <div>
-            chua login
-        </div>
-    );
+  const [show, setShow] = useState(false);
+  const handleShowPopup = () => {
+    setShow(true);
+  };
+  const handleClosePopup = () => {
+    setShow(false);
+  };
+  return (
+    <div>
+      <Form>
+        <Button onClick={handleShowPopup}>Danh sách dịch vụ</Button>
+        <TableServices show={show} onClosed={handleClosePopup} />
+        <FloatingLabel label="Email">
+          <Form.Control type="email" />
+        </FloatingLabel>
+        <FloatingLabel label="Họ và tên">
+          <Form.Control type="text" />
+        </FloatingLabel>
+        <FloatingLabel label="SĐT">
+          <Form.Control type="text" />
+        </FloatingLabel>
+        <FloatingLabel label="Địa chỉ">
+          <Form.Control as="textarea" cols="4" />
+        </FloatingLabel>
+        <FloatingLabel label="Boss của bạn thuộc">
+          <Form.Select>
+            <option></option>
+          </Form.Select>
+        </FloatingLabel>
+        <FloatingLabel label="Tên của boss">
+          <Form.Control type="text" />
+        </FloatingLabel>
+        <FloatingLabel label="Ngày làm dịch vụ">
+          <Form.Control type="date" />
+        </FloatingLabel>
+        <FloatingLabel label="Thời gian làm dịch vụ">
+          <Form.Control type="time" />
+        </FloatingLabel>
+        <ButtonGroup>
+          <Button type="submit">Gửi yêu cầu</Button>
+          <Button variant="warning" type="reset">
+            Chọn lại
+          </Button>
+        </ButtonGroup>
+      </Form>
+    </div>
+  );
 };
 
 export default NotLogin;
