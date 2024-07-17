451eab8dec5874c1785a7493e397d14a71457335
phduo <phduo@DUONGPHAM>
2024-07-17 17:01:59 +0700

    duong commit

--- src/pages/DichVu/DichVu.jsx
+++ src/pages/DichVu/DichVu.jsx
@@ -1,10 +1,21 @@
+import { Container, Row } from "react-bootstrap";
+import "./DichVu.css";
 
 const DichVu = () => {
-    return (
-        <div>
-            <h1>Trang dich vu</h1>            
-        </div>
-    );
+  return (
+    <Container className="services">
+      <Row className="row-1">
+        <h3>Dịch vụ</h3>
+      </Row>
+      <Row className="row-2">
+        <p>
+          Tại MewShop, đội ngũ nhân viên của chúng tôi được đào tạo để nâng cao
+          năng lực chuyên môn với cơ sở vật chất hiện đại nhằm duy trì tiêu
+          chuẩn cao trong công tác chăm sóc sức khỏe của các Boss
+        </p>
+      </Row>
+    </Container>
+  );
 };
 
 export default DichVu;
