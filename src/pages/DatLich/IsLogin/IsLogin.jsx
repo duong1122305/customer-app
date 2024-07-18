6048ee68179649733613195ea5c081b8170903b4
duong pham <phduongc1tantrao2018@gmail.com>
2024-07-19 00:52:16 +0700

    call api booking

--- src/pages/DatLich/IsLogin/IsLogin.jsx
+++ src/pages/DatLich/IsLogin/IsLogin.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button, ButtonGroup, FloatingLabel, Form } from "react-bootstrap";
 import TableServices from "../TableServices/TableServices";
 import "./IsLogin.css";
@@ -9,6 +9,25 @@ const IsLogin = () => {
   const [selectedServicesForForm, setSelectedServicesForForm] = useState([]);
   const [show, setShow] = useState(false);
   const [showPet, setShowPet] = useState(false);
+
+  //ref
+  const petIdRef = useRef(null);
+  const startDateTimeRef = useRef(null);
+  const dateBookingRef = useRef(null);
+  const voucherIdRef = useRef(null);
+
+  const token = sessionStorage.getItem("token");
+  const result = JSON.parse(token);
+  const id = result.id;
+  const [data, setData] = useState({
+    petId: 0,
+    serviceDetailId: [],
+    startDateTime: "",
+    dateBooking: "",
+    voucherId: 0,
+    guestId: id,
+  });
+
   const handleShowPopup = () => {
     setShow(true);
   };
@@ -20,9 +39,6 @@ const IsLogin = () => {
     handleClosePopup(); // Đóng modal sau khi chọn
   };
   useEffect(() => {
-    const token = sessionStorage.getItem("token");
-    const result = JSON.parse(token);
-    const id = result.id;
     const getPet = async () => {
       const response = await fetch(
         `https://localhost:7039/api/PetManager/get-pet-by-guest?id=${id}`,
@@ -40,39 +56,88 @@ const IsLogin = () => {
         console.log("Khong co data");
       }
     };
+
     getPet();
-  }, []);
+  }, [id]);
+
   const handleCreatePet = () => {
     setShowPet(true);
   };
+
+  const handleBooking = async (event) => {
+    event.preventDefault();
+
+    setData({
+      petId: parseInt(petIdRef.current.value),
+      startDateTime: startDateTimeRef.current.value, // Lấy giá trị trực tiếp
+      dateBooking: dateBookingRef.current.value, // Lấy giá trị trực tiếp
+      serviceDetailId:
+        selectedServicesForForm.length > 0 ? selectedServicesForForm : [],
+    });
+    try {
+      const response = await fetch(
+        "https://localhost:7039/api/Booking/Guest-Booking",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const result = await response.json();
+      if (result.isSuccess === true) {
+        console.log("Booking successful:", result);
+      } else {
+        console.error("Booking failed:", result.error);
+      }
+    } catch (error) {
+      console.error("Error occurred during booking:", error);
+    }
+  };
+
   console.log(selectedServicesForForm);
+
   return (
     <div>
-      <Form className="form_booking">
+      <Form className="form_booking" onSubmit={handleBooking}>
         <Button onClick={handleShowPopup}>Chọn dịch vụ</Button>
         <TableServices
           show={show}
           onClosed={handleClosePopup}
           onServicesSelected={handleServicesSelected}
         />
-        <input type="hidden" name="selectedServices" value={JSON.stringify(selectedServicesForForm)} />
+        <input
+          type="hidden"
+          name="selectedServices"
+          value={JSON.stringify(selectedServicesForForm)}
+        />
         <FloatingLabel label="Boss hưởng thụ">
-          <Form.Select>
+          <Form.Select ref={petIdRef}>
             {lstPet.length > 0 ? (
-              lstPet.map((pet) => <option key={pet.id}>{pet.name}</option>)
+              lstPet.map((pet) => (
+                <option key={pet.id} value={pet.id}>
+                  {pet.name}
+                </option>
+              ))
             ) : (
               <option>Bạn chưa thêm Boss của mình rùi !!!</option>
             )}
           </Form.Select>
         </FloatingLabel>
         <FloatingLabel label="Ngày làm dịch vụ">
-          <Form.Control type="date" />
+          <Form.Control type="date" ref={dateBookingRef} />
         </FloatingLabel>
         <FloatingLabel label="Giờ làm dịch vụ">
-          <Form.Control type="time" />
+          <Form.Control type="time" ref={startDateTimeRef} />
         </FloatingLabel>
         <FloatingLabel label="Bạn có voucher chứ">
-          <Form.Control type="text" placeholder="X3aBdcss4" />
+          <Form.Control
+            type="text"
+            placeholder="X3aBdcss4"
+            ref={voucherIdRef}
+          />
         </FloatingLabel>
         <ButtonGroup>
           <Button type="submit">Gửi yêu cầu</Button>
